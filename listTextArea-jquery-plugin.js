ef8995562057d2bf5f68e86822b68511d7b91062
benjamin <jjdoor@163.com>
2023-04-25 10:43:28 +0800

    使用类似vue的双向绑定机制实现数据动态更新

--- listTextArea-jquery-plugin.js
+++ listTextArea-jquery-plugin.js
@@ -9,32 +9,15 @@ $(document).ready(function() {
     }
     $('.list-container').html(arrayHtml);
 
-    // 将选中的数组项添加到文本框中
-    $('.array-item').on('click', function() {
-        //已经有fontline-line-through样式的，对应的文本框中的值已经添加过了，不需要再添加
-        if ($(this).hasClass('fontline-line-through')) {
-            return false;
-        }
-        var value = $(this).text();
-        var textInput = $('.list-string');
-        var currentVal = textInput.val();
-
-        if (currentVal === "") {
-            textInput.val(value);
-        } else {
-            textInput.val(currentVal + "," + value);
-        }
-
-        //将选中的数组项加上删除样式
-        $(this).addClass('fontline-line-through');
-        // $(this).remove();
-    });
-
-// 将文本框中的值添加回数组中
+    // 将文本框中的值添加回数组中
     $('.list-string').click(function() {
         var textInput = $(this);
         var text = textInput.val();
         var clickedIndex = event.target.selectionStart;
+        //点击最后一个字符不做处理，以便用户点击输入字符
+        if (clickedIndex === text.length) {
+            return;
+        }
         //以clickefIndex为中心点，向左向右找逗号，如果找不到逗号，就是整个字符串
         var leftIndex = text.lastIndexOf(",", clickedIndex);
         var rightIndex = text.indexOf(",", clickedIndex);
@@ -80,76 +63,82 @@ $(document).ready(function() {
                 var currentItemText = currentItem.text();
 
                 if (currentItemText === subStr) {
-                    // currentItem.text("");
                     currentItem.removeClass('fontline-line-through');
                     break;
                 }
             }
-
-
-
-            // var newItem = $('<div class="array-item"></div>');
-
-
-            // newItem.text(subStr);
-            // arrayContainer.append(newItem);
         }
 
         return ;
-        console.log("Clicked character index: " + clickedIndex);
-
-
-
-        var textInput = $(this);
-        var value = textInput.val();
-        var startIndex = textInput[0].selectionStart;
-        var endIndex = textInput[0].selectionEnd;
-
-        var subStr = value.substring(0, startIndex);
-        var idx = subStr.lastIndexOf(",");
-        if (idx !== -1) {
-            subStr = subStr.substring(idx + 1);
-        }
-        subStr += value.substring(endIndex);
-
-        if (subStr !== "") {
-            var arrayContainer = $('.list-container');
-            var items = arrayContainer.find('.array-item');
-            var inserted = false;
-
-            for (var i = 0; i < items.length; i++) {
-                var currentItem = items.eq(i);
-                var currentItemText = currentItem.text();
-
-                if (currentItemText === "") {
-                    currentItem.text(subStr);
-                    inserted = true;
-                    break;
-                } else if (currentItemText.indexOf(',') > -1) {
-                    var subItems = currentItemText.split(',');
-                    if (subItems[0] === subStr) {
-                        currentItem.text(subItems[1]);
-                        inserted = true;
-                        break;
-                    } else if (subItems[1] === subStr) {
-                        currentItem.text(subItems[0]);
-                        inserted = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!inserted) {
-                arrayContainer.append('<div class="array-item">' + subStr + '</div>');
-            }
-        }
-        textInput.val('');
     });
+});
 
-//给jquery添加一个方法，用于清除fontline-line-through样式
+//jquery立即执行函数
+$(function() {
     $.fn.removeFontline = function() {
         return $('.array-item').each(function() {
             $(this).removeClass('fontline-line-through');
         });
     }
+    // 在文档加载完成后执行这里的代码
+    //给jquery添加一个方法，用于清除fontline-line-through样式
+    $.fn.listTextAreaClear = function() {
+        return $('.array-item').each(function() {
+            $(this).removeClass('fontline-line-through');
+        });
+    };
+
+    // 获取 list-container 元素和 array-item 元素
+    var $listContainer = $('.list-container');
+    var $arrayItems = $listContainer.find('.array-item');
+    // 监听 list-container 元素的属性变化
+    var observer = new MutationObserver(function(mutations) {
+        // 获取新的 data-list 属性值，并使用逗号进行分割
+        var dataList = $listContainer.attr('data-list').split(',');
+
+        // 如果数据项数量和元素数量不一致，则重新渲染元素
+        // if (dataList.length !== $arrayItems.length) {
+        // 先清空所有的 array-item 元素
+        $arrayItems.remove();
+
+        // 遍历数据项，创建新的 array-item 元素并添加到容器中
+        for (var i = 0; i < dataList.length; i++) {
+            $('<div class="array-item"></div>').text(dataList[i]).appendTo($listContainer);
+        }
+
+        // 更新 array-item 元素列表
+        $arrayItems = $listContainer.find('.array-item');
+        // }
+    });
+
+    // 开始监听 list-container 元素的属性变化
+    observer.observe($listContainer[0], { attributes: true });
+
+    // 监听 array-item 元素的 click 事件
+    $listContainer.on('click', '.array-item', function(e) {
+        // 获取当前的数据项和下标
+        var data = $(e.target).text();
+        var index = $(e.target).index();
+
+        // 弹出提示框，显示当前数据项和下标
+        // alert('当前数据项为：' + data + '，下标为：' + index);
+
+
+        //已经有fontline-line-through样式的，对应的文本框中的值已经添加过了，不需要再添加
+        if ($(this).hasClass('fontline-line-through')) {
+            return false;
+        }
+        var value = $(this).text();
+        var textInput = $('.list-string');
+        var currentVal = textInput.val();
+
+        if (currentVal === "") {
+            textInput.val(value);
+        } else {
+            textInput.val(currentVal + "," + value);
+        }
+
+        //将选中的数组项加上删除样式
+        $(this).addClass('fontline-line-through');
+    });
 });
